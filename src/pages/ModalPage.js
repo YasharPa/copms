127c7846958c8f4b136944a68589b8f4eda9af4c
YasharPa <yasharpashaiy1@gmail.com>
2023-06-10 16:16:13 +0300

    bug fixed.

--- src/pages/ModalPage.js
+++ src/pages/ModalPage.js
@@ -29,6 +29,116 @@ function ModalPage() {
         Open Modal
       </Button>
       {showModal && modal}
+      <p>
+        "English learning apps are a convenient and effective way to improve
+        your language skills. Duolingo, Babbel, Rosetta Stone, and Memrise are
+        popular options that offer interactive lessons, covering vocabulary,
+        grammar, speaking, listening, and writing. HelloTalk and Tandem connect
+        you with native English speakers for language exchange. FluentU utilizes
+        authentic videos, while Busuu provides personalized study plans.
+        Additionally, platforms like EnglishClass101 and Lingoda offer online
+        classes with qualified teachers. Explore these apps to find the one that
+        suits your learning style and goals."
+      </p>
+      <p>
+        "English learning apps are a convenient and effective way to improve
+        your language skills. Duolingo, Babbel, Rosetta Stone, and Memrise are
+        popular options that offer interactive lessons, covering vocabulary,
+        grammar, speaking, listening, and writing. HelloTalk and Tandem connect
+        you with native English speakers for language exchange. FluentU utilizes
+        authentic videos, while Busuu provides personalized study plans.
+        Additionally, platforms like EnglishClass101 and Lingoda offer online
+        classes with qualified teachers. Explore these apps to find the one that
+        suits your learning style and goals."
+      </p>
+      <p>
+        "English learning apps are a convenient and effective way to improve
+        your language skills. Duolingo, Babbel, Rosetta Stone, and Memrise are
+        popular options that offer interactive lessons, covering vocabulary,
+        grammar, speaking, listening, and writing. HelloTalk and Tandem connect
+        you with native English speakers for language exchange. FluentU utilizes
+        authentic videos, while Busuu provides personalized study plans.
+        Additionally, platforms like EnglishClass101 and Lingoda offer online
+        classes with qualified teachers. Explore these apps to find the one that
+        suits your learning style and goals."
+      </p>
+      <p>
+        "English learning apps are a convenient and effective way to improve
+        your language skills. Duolingo, Babbel, Rosetta Stone, and Memrise are
+        popular options that offer interactive lessons, covering vocabulary,
+        grammar, speaking, listening, and writing. HelloTalk and Tandem connect
+        you with native English speakers for language exchange. FluentU utilizes
+        authentic videos, while Busuu provides personalized study plans.
+        Additionally, platforms like EnglishClass101 and Lingoda offer online
+        classes with qualified teachers. Explore these apps to find the one that
+        suits your learning style and goals."
+      </p>
+      <p>
+        "English learning apps are a convenient and effective way to improve
+        your language skills. Duolingo, Babbel, Rosetta Stone, and Memrise are
+        popular options that offer interactive lessons, covering vocabulary,
+        grammar, speaking, listening, and writing. HelloTalk and Tandem connect
+        you with native English speakers for language exchange. FluentU utilizes
+        authentic videos, while Busuu provides personalized study plans.
+        Additionally, platforms like EnglishClass101 and Lingoda offer online
+        classes with qualified teachers. Explore these apps to find the one that
+        suits your learning style and goals."
+      </p>
+      <p>
+        "English learning apps are a convenient and effective way to improve
+        your language skills. Duolingo, Babbel, Rosetta Stone, and Memrise are
+        popular options that offer interactive lessons, covering vocabulary,
+        grammar, speaking, listening, and writing. HelloTalk and Tandem connect
+        you with native English speakers for language exchange. FluentU utilizes
+        authentic videos, while Busuu provides personalized study plans.
+        Additionally, platforms like EnglishClass101 and Lingoda offer online
+        classes with qualified teachers. Explore these apps to find the one that
+        suits your learning style and goals."
+      </p>
+      <p>
+        "English learning apps are a convenient and effective way to improve
+        your language skills. Duolingo, Babbel, Rosetta Stone, and Memrise are
+        popular options that offer interactive lessons, covering vocabulary,
+        grammar, speaking, listening, and writing. HelloTalk and Tandem connect
+        you with native English speakers for language exchange. FluentU utilizes
+        authentic videos, while Busuu provides personalized study plans.
+        Additionally, platforms like EnglishClass101 and Lingoda offer online
+        classes with qualified teachers. Explore these apps to find the one that
+        suits your learning style and goals."
+      </p>
+      <p>
+        "English learning apps are a convenient and effective way to improve
+        your language skills. Duolingo, Babbel, Rosetta Stone, and Memrise are
+        popular options that offer interactive lessons, covering vocabulary,
+        grammar, speaking, listening, and writing. HelloTalk and Tandem connect
+        you with native English speakers for language exchange. FluentU utilizes
+        authentic videos, while Busuu provides personalized study plans.
+        Additionally, platforms like EnglishClass101 and Lingoda offer online
+        classes with qualified teachers. Explore these apps to find the one that
+        suits your learning style and goals."
+      </p>
+      <p>
+        "English learning apps are a convenient and effective way to improve
+        your language skills. Duolingo, Babbel, Rosetta Stone, and Memrise are
+        popular options that offer interactive lessons, covering vocabulary,
+        grammar, speaking, listening, and writing. HelloTalk and Tandem connect
+        you with native English speakers for language exchange. FluentU utilizes
+        authentic videos, while Busuu provides personalized study plans.
+        Additionally, platforms like EnglishClass101 and Lingoda offer online
+        classes with qualified teachers. Explore these apps to find the one that
+        suits your learning style and goals."
+      </p>
+      <p>
+        "English learning apps are a convenient and effective way to improve
+        your language skills. Duolingo, Babbel, Rosetta Stone, and Memrise are
+        popular options that offer interactive lessons, covering vocabulary,
+        grammar, speaking, listening, and writing. HelloTalk and Tandem connect
+        you with native English speakers for language exchange. FluentU utilizes
+        authentic videos, while Busuu provides personalized study plans.
+        Additionally, platforms like EnglishClass101 and Lingoda offer online
+        classes with qualified teachers. Explore these apps to find the one that
+        suits your learning style and goals."
+      </p>
     </div>
   );
 }
